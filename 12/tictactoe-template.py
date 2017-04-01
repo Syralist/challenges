f47f8e554dd53501723caadf221b224f9d43485d
Thomas Helmke <thomas.helmke@gmx.de>
2017-04-01 13:03:00 +0200

    check for win and draw, player class, stub for AI class

--- 12/tictactoe-template.py
+++ 12/tictactoe-template.py
@@ -6,6 +6,12 @@ WINNING_COMBINATIONS = (
     (1, 5, 9), (7, 5, 3),
 )
 
+DEBUG = True
+
+def clear():
+    if not DEBUG:
+        print("\n" * 100)
+
 
 class TicTacToe:
 
@@ -19,15 +25,39 @@ class TicTacToe:
 
     def setPosition(self, pos, token):
         try:
-            if self.board[pos] == DEFAULT:
+            # if self.board[pos] == DEFAULT:
+            if pos in VALID_POSITIONS:
                 self.board[pos] = token
+                VALID_POSITIONS.remove(pos)
                 return True
             else:
-                print "Position already taken"
+                print("Position invalid: ", pos) 
                 return False
         except IndexError as e:
-            print "invalid Position: ", pos 
+            print("invalid Position: ", pos) 
             return False
+
+    def is_win(self, token):
+        for comb in WINNING_COMBINATIONS:
+            num = 0
+            for p in comb:
+                if self.board[p] == DEFAULT:
+                    break
+                elif self.board[p] == token:
+                    num += 1
+
+            if num == 3:
+                return True
+
+        return False
+
+    def is_draw(self):
+        if len(VALID_POSITIONS > 0):
+            return False
+        else:
+            return True
+
+
 
     # TODOS:
     # display board in __str__ (clearing screen)
@@ -37,16 +67,59 @@ class TicTacToe:
     # if playing against computer (AI) calculate next best move (algorithm)
     # update board upon each turn
 
+class Player:
+    def __init__(self, game, token):
+        self.game = game
+        self.token = token
+
+    def move(self):
+        pos = int(input("where to draw?"))
+        while not game.setPosition(pos, self.token):
+            pos = int(input("where to draw?"))
+
+class AI:
+    def __init__(self, game, token, level):
+        self.game = game
+        self.token = token
+        self.level = level
+
 
 if __name__ == "__main__":
+    game = TicTacToe()
+    player1 = Player(game, "X")
     while True:
-        game = TicTacToe()
-        print game
-        game.setPosition(1, "X")
-        print game
-        game.setPosition(11, "O")
-        print game
-        break
+        clear()
+        print(game)
+        player1.move()
+        if game.is_win("X"):
+            print("player won!")
+            break
+        elif game.is_draw():
+            print("its a tie!")
+            break
+        # game.setPosition(1, "X")
+        # clear()
+        # print(game)
+        # game.setPosition(1, "O")
+        # clear()
+        # print(game)
+        # game.setPosition(2, "O")
+        # clear()
+        # print(game)
+        # print(game.is_win("O"))
+        # player1.move()
+        # game.setPosition(4, "X")
+        # clear()
+        # print(game)
+        # game.setPosition(5, "O")
+        # clear()
+        # print(game)
+        # player1.move()
+        # game.setPosition(7, "X")
+        # clear()
+        # print(game)
+        # print(game.is_win("X"))
+        # break
         # take turn
         # make move
         # check win - break
